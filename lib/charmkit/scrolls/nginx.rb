d0a7493d4832de2588a136afb8b312569b8a1158
Adam Stokes <battlemidget@users.noreply.github.com>
2016-12-22 23:44:35 -0500

    init
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

---
+++ lib/charmkit/scrolls/nginx.rb
@@ -0,0 +1,26 @@
+require "charmkit"
+
+class Nginx < Charmkit::Scroll
+  depends_on "nginx-full"
+
+  def add_host(server_name, application_type, template = 'vhost.conf')
+    case application_type
+    when :php
+      inline_template 'php.vhost.conf', "/etc/nginx/sites-enabled/#{server_name}",
+                      app_path: '/srv/app', server_name: server_name
+    when :nodejs
+      inline_template 'nodejs.vhost.conf', "/etc/nginx/sites-enabled/#{server_name}",
+                      app_path: '/srv/app', server_name: server_name
+    end
+  end
+end
+
+__END__
+
+@@ php.vhost.conf
+
+suitable nginx host configuration for a php application
+
+@@ nodejs.vhost.conf
+
+suitable nginx host configuration for a nodejs application
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'charmkit/scroll/nginx'
+
+require 'minitest/autorun'
